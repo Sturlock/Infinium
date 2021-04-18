444242363cae6e23be1689f1fd704cf818366e9e
Alomodo <ellentaylor26.et@gmail.com>
2021-04-18 14:16:46 +0100

    Merging Menus v1
    Version 1: Audio WIP

---
+++ Infinium_Testing/Assets/Scripts/UI/AudioManager.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string BGSoundeffectPref = "BGSoundeffectPref";
+    private static readonly string SoundeffectPref = "SoundeffectPref";
+
+    private int firstPlayInt;
+    public Slider backgroundSlider, bgsfxSlider, sfxSlider;
+    private float backroundFloat, bgsfxFloat, sfxFloat;
+
+    //public Sound[] sounds;
+    public int scene = 0;
+
+    public AudioSource[] BackgroundAudio;
+    public AudioSource[] BGsoundeffectAudio;
+    public AudioSource[] SoundeffectAudio;
+
+    public static AudioManager Instance;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene().buildIndex;
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        if(firstPlayInt == 0)
+        {
+            backroundFloat = .5f;
+            bgsfxFloat = .8f;
+            sfxFloat = 1f;
+            backgroundSlider.value = backroundFloat;
+            bgsfxSlider.value = bgsfxFloat;
+            sfxSlider.value = sfxFloat;
+
+            PlayerPrefs.SetFloat(BackgroundPref, backroundFloat);
+            PlayerPrefs.SetFloat(BGSoundeffectPref, bgsfxFloat);
+            PlayerPrefs.SetFloat(SoundeffectPref, sfxFloat);
+
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+           backroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backroundFloat;
+            bgsfxFloat = PlayerPrefs.GetFloat(BGSoundeffectPref);
+            bgsfxSlider.value = bgsfxFloat;
+            sfxFloat = PlayerPrefs.GetFloat(SoundeffectPref);
+            sfxSlider.value = sfxFloat;
+        }
+        for (int i = 0; i < BackgroundAudio.Length; i++)
+        {
+            //Debug.Log(i);
+            if (scene == i)
+            {
+                BackgroundAudio[i].Play();
+            }
+            else
+            {
+                BackgroundAudio[i].Stop();
+            }
+        }
+        for (int i = 0; i < BGsoundeffectAudio.Length; i++)
+        {
+            if (scene == i)
+            {
+                BGsoundeffectAudio[i].Play();
+            }
+            else
+            {
+                BGsoundeffectAudio[i].Stop();
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if (scene != SceneManager.GetActiveScene().buildIndex)
+        {
+            scene = SceneManager.GetActiveScene().buildIndex;
+            for (int i = 0; i < BackgroundAudio.Length; i++)
+            {
+                Debug.Log(i);
+                if (scene == i)
+                {
+                    BackgroundAudio[i].Play();
+                }
+                else
+                {
+                    BackgroundAudio[i].Stop();
+                }
+            }
+
+            for (int i = 0; i < BGsoundeffectAudio.Length; i++)
+            {
+                if (scene == i)
+                {
+                    BGsoundeffectAudio[i].Play();
+                }
+                else
+                {
+                    BGsoundeffectAudio[i].Stop();
+                }
+            }
+        }
+    }
+
+
+    public void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SaveSoundSettings()
+    {
+       PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+       PlayerPrefs.SetFloat(BGSoundeffectPref, bgsfxSlider.value);
+       PlayerPrefs.SetFloat(SoundeffectPref, sfxSlider.value);
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (!focus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    public void UpdateSound()
+    {
+        for (int i = 0; i < BackgroundAudio.Length; i++)
+        {
+            BackgroundAudio[i].volume = backgroundSlider.value;
+        }
+        for (int i = 0; i < BGsoundeffectAudio.Length; i++)
+        {
+            BGsoundeffectAudio[i].volume = bgsfxSlider.value;
+        }
+        for(int i = 0; i < SoundeffectAudio.Length; i++)
+        {
+            SoundeffectAudio[i].volume = sfxSlider.value;
+        }
+    }
+}
