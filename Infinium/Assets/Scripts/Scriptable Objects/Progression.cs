d551bb3aef738a9a568c712953fd4ccfa202fcc2
Sturlock <willsturley@yahoo.co.uk>
2021-05-10 01:25:25 +0100

    Progression Start

---
+++ Infinium/Assets/Scripts/Scriptable Objects/Progression.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Progression", menuName = "Infinium/Progression", order =0)]
+public class Progression : ScriptableObject
+{
+
+}
