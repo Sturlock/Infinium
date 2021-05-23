799cbed28b6ed8563d311480e1846974a739ef59
Sturlock <willsturley@yahoo.co.uk>
2021-05-23 17:30:49 +0100

    Quick fixes

--- Infinium/Assets/Scripts/Core/Player/Testing/Transition.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d49b7c078238d2595c2fea8941f4d6e6a66d63837002b0b4f56cfda46ec277d9
-size 4617
--- Infinium/Assets/Scripts/Core/Player/Transition.cs
+++ Infinium/Assets/Scripts/Core/Player/Transition.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d963273ed7e8138c99acac7a69bef0674470fac2c92d5264a8f0c25c21a664de
-size 4895
+oid sha256:e6332bb36230ac397b1bafa4b8ff59d0793030231efaadaf88ced538b515bab3
+size 4912
