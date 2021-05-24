4398b22b9f23a9800a372daed7332c232f9db583
Sturlock <willsturley@yahoo.co.uk>
2021-05-24 01:28:52 +0100

    Docking & Death

--- Infinium/Assets/Scripts/Core/Player/Transition.cs
+++ Infinium/Assets/Scripts/Core/Player/Transition.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:330eb6ef2d8cee2e712afe9faaf9c3873d512abafd3d890a9b551b948a94b573
-size 5079
+oid sha256:e403ff21255ccd918ee4c31ec5785de238ef4fa251a2bd4e1dd8995d053e7ff9
+size 5178
