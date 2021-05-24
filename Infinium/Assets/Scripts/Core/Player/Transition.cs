764a0123d4804d4d1086124ac8f4df23c4e4c7ef
Sturlock <willsturley@yahoo.co.uk>
2021-05-24 03:15:27 +0100

    SpeedOmeter working

--- Infinium/Assets/Scripts/Core/Player/Transition.cs
+++ Infinium/Assets/Scripts/Core/Player/Transition.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e403ff21255ccd918ee4c31ec5785de238ef4fa251a2bd4e1dd8995d053e7ff9
-size 5178
+oid sha256:507e74f732f0559806c544d78959be29c9a5e73a280a6f18c03f1be31318e79d
+size 6205
